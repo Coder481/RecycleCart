ed3851b126ea8f8855c2e908b5b237d05a19e8a9
Coder481 <hrithiksharma24092000@gmail.com>
2020-11-08 10:32:36 +0530

    Update Commit

---
+++ app/src/main/java/com/example/recyclecart/ProductEditorDialog.java
@@ -0,0 +1,168 @@
+package com.example.recyclecart;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.recyclecart.databinding.DialogProductEditBinding;
+import com.example.recyclecart.models.Product;
+
+import java.util.regex.Pattern;
+
+import static com.example.recyclecart.models.Product.VARIANTS_BASED;
+import static com.example.recyclecart.models.Product.WEIGHT_BASED;
+
+public class ProductEditorDialog {
+
+    private DialogProductEditBinding b;
+    private Product product;
+
+    void show(final Context context, final Product product, final OnProductEditedListener listener){
+
+        // Inflate
+        b = DialogProductEditBinding.inflate(LayoutInflater.from(context));
+        this.product = product;
+
+        // Show Dialog when OptionsMenu (+) button is clicked
+        new AlertDialog.Builder(context)
+                .setTitle("Edit Product")
+                .setView(b.getRoot())
+                .setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (areProductDetailsValid())
+                            listener.onProductEdited(ProductEditorDialog.this.product);
+
+                        else
+                            Toast.makeText(context,"Invalid Details!!",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.onCancelled();
+                    }
+                })
+                .show();
+
+        setupRadioButton();
+        preFillPreviousDetails();
+
+    }
+
+    // Show previously filled details when Edit button (in contextual Menu) is clicked
+    private void preFillPreviousDetails() {
+        // Set name
+        b.name.setText(product.name);
+
+        // Change Radio Selection
+        b.productType.check(product.type == WEIGHT_BASED ? R.id.weightBasedRadioBtn : R.id.variantBasedRadioBtn);
+
+        // Setup views according to type of Product
+        if (product.type == WEIGHT_BASED){
+            b.pricePerKgEditText.setText(product.pricePerkg+"");
+            b.minQtyEditText.setText(product.minQtyToString());
+        }
+        else{
+            b.variantsEditText.setText(product.variantsString());
+        }
+    }
+
+
+    // Set visibility of either Variants or WeightBased
+    private void setupRadioButton() {
+        b.productType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.weightBasedRadioBtn){
+                    b.weightBasedMinQtyRoot.setVisibility(View.VISIBLE);
+                    b.weightBasedPricePerKgRoot.setVisibility(View.VISIBLE);
+                    b.variantsRoot.setVisibility(View.GONE);
+                }
+                else{
+                    b.variantsRoot.setVisibility(View.VISIBLE);
+                    b.weightBasedMinQtyRoot.setVisibility(View.GONE);
+                    b.weightBasedPricePerKgRoot.setVisibility(View.GONE); }
+            }
+        });
+    }
+
+    // Check for Product Details Validation
+    private boolean areProductDetailsValid() {
+        // checking for name
+        String name = b.name.getText().toString().trim();
+        if (name.isEmpty())
+            return false;
+
+        // Set name to product
+        product.name=name;
+
+
+        switch(b.productType.getCheckedRadioButtonId()){
+            case R.id.weightBasedRadioBtn:
+                // Set type
+                product.type=WEIGHT_BASED;
+
+                // Get Values from Views
+                String pricePerKg = b.pricePerKgEditText.getText().toString().trim()
+                        , minQty = b.minQtyEditText.getText().toString().trim();
+                // Check inputs
+                if(pricePerKg.isEmpty() || minQty.isEmpty() || !minQty.matches("\\d+(kg|g)"))
+                    return false;
+
+                // If All Good,set Values to the product
+                product = new Product(name
+                        ,Integer.parseInt(pricePerKg)
+                        ,extractMinQtyFromString(minQty));
+
+                return true;
+
+            case R.id.variantBasedRadioBtn:
+
+                // Get Values from views
+                String variants = b.variantsEditText.getText().toString().trim();
+
+                product = new Product(name);
+                return areVariantsValid(variants);
+        }
+        return  false;
+    }
+
+    // Return float value of MinQty from string like "12kg" "400g"
+    private float extractMinQtyFromString(String minQty) {
+
+        /*if(minQty.contains("kg") || minQty.contains("Kg") || minQty.contains("KG"))
+            return Integer.parseInt(minQty.replaceAll("kg|KG|Kg",""));*/
+        if(minQty.contains("kg"))
+            return Integer.parseInt(minQty.replace("kg",""));
+        else
+            return Integer.parseInt(minQty.replace("g",""))/1000f;
+    }
+
+    // Checks for valid variants
+    private boolean areVariantsValid(String variants) {
+        if (variants.length()==0)
+            return false;
+
+        String[] vs = variants.split("\n");
+        Pattern pattern = Pattern.compile("^\\w+(\\s|\\w)+,(\\s+\\d+$|\\d+$)");
+        for (String variant : vs) {
+            if (!pattern.matcher(variant).matches())
+                return false;
+        }
+
+        // Extracts Variants from String[]
+        product.fromVariantStrings(vs);
+        return true;
+    }
+
+    // Listener Interface to notify Activity of Dialog events
+    interface OnProductEditedListener{
+        void onProductEdited(Product product);
+        void onCancelled();
+    }
+}
--- app/src/main/java/com/example/recyclecart/ProductsAdapter.java
+++ app/src/main/java/com/example/recyclecart/ProductsAdapter.java
@@ -1,95 +1,151 @@
 package com.example.recyclecart;
 
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.recyclecart.databinding.ProductItemBinding;
+import com.example.recyclecart.databinding.VariantBasedProductBinding;
+import com.example.recyclecart.databinding.WeightBasedProductBinding;
 import com.example.recyclecart.models.Product;
 
 import java.util.List;
 
 // Adapter for List of Products
-public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHolder> {
+public class ProductsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    // Needed for Inflating Data
     private Context context;
+
+    // List of Data
     private List<Product> productList;
+
+    int lastSelectedItemPosition;
 
     public ProductsAdapter(Context context , List<Product> productList){
         this.context = context;
         this.productList = productList;
     }
 
-    // Inflate the view for item abd create a ViewHolder object( View to Java Code)
+    // Inflate the view for item and create a ViewHolder object based on ViewType( View to Java Code)
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //1. Inflate the layout for product_item.xml
-        ProductItemBinding b = ProductItemBinding.inflate(LayoutInflater.from(context)
-                ,parent ,
-                false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        //2. Create ViewHolder object  and return
-        return new ViewHolder(b);
+        if(viewType==Product.WEIGHT_BASED){
+            // Inflate the layout for weight_based_product.xml
+            WeightBasedProductBinding b = WeightBasedProductBinding.inflate(
+                    LayoutInflater.from(context)
+                    ,parent
+                    ,false);
+
+            // Create and return WeightBasedProductViewHolder
+            // Child -> Parent
+            return new WeightBasedProductViewHolder(b);
+        }else{
+            // Inflate the Layout for variant_based_product.xml
+            VariantBasedProductBinding b = VariantBasedProductBinding.inflate(LayoutInflater.from(context)
+            ,parent
+            ,false);
+
+            // Create and Return VariantBasedProductViewHolder
+            return new VariantBasedProductViewHolder(b);
+        }
+
     }
+
+
+
+    // Return ViewType Based on position
+    @Override
+    public int getItemViewType(int position) {
+        return productList.get(position).type;
+    }
+
+
 
     // Binds the data to view
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
         // Get the data at position
         final Product product = productList.get(position);
 
-        //Bind data
-        // Name and Price
-        holder.b.name.setText(String.format("%s (Rs. %d)",product.name,product.price));
+        if (product.type == Product.WEIGHT_BASED){
+            //Get Binding
+            WeightBasedProductBinding b = ((WeightBasedProductViewHolder) holder).b;
 
-        // Quantity
-        holder.b.quantity.setText(product.qty+"");
+            //Bind data
+            b.weightBasedProductName.setText(product.name);
+            b.weightBasedPricePerKg.setText("Rs. "+product.pricePerkg+"/kg");
+            b.weightBasedMinQty.setText("MinQty-"+product.minQty);
 
-        // decrementButton &  Quantity TV Visibility
-        holder.b.decrementBtn.setVisibility(product.qty > 0 ? View.VISIBLE : View.GONE);
-        holder.b.quantity.setVisibility(product.qty > 0 ? View.VISIBLE : View.GONE);
+            // Setup Contextual Menu inflation
+            setupContextMenu(b.getRoot());
 
-        // Configure increment
-        holder.b.incrementBtn.setOnClickListener(new View.OnClickListener(){
+        }else{
+            // Get Binding
+            VariantBasedProductBinding b = ((VariantBasedProductViewHolder) holder).b;
 
+            // Bind Data
+            b.variantBasedProductName.setText(product.name);
+            b.variantsBasedVariantName.setText(product.variantsString());
+
+            // Setup Contextual Menu inflation
+            setupContextMenu(b.getRoot());
+        }
+
+        // Save dynamic position of selected item to access it in Activity
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                product.qty++;
-                notifyItemChanged(position);
+            public boolean onLongClick(View v) {
+                lastSelectedItemPosition = holder.getAdapterPosition();
+                return false;
             }
         });
-        // Configure decrement
-        holder.b.decrementBtn.setOnClickListener(new View.OnClickListener(){
 
+
+    }
+
+    private void setupContextMenu(ConstraintLayout root) {
+        root.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
             @Override
-            public void onClick(View view) {
-                product.qty--;
-                notifyItemChanged(position);
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+                if(!(context instanceof CatalogActivity))
+                    return;
+                ((CatalogActivity) context).getMenuInflater().inflate(R.menu.product_contextual_menu,menu);
             }
         });
     }
 
+
+    // Recycler View get to know how many Data are there to bind
     @Override
     public int getItemCount() {
         return productList.size();
     }
 
-    // Holds the view for each item
-    public class ViewHolder extends RecyclerView.ViewHolder{
 
-
-        private com.example.recyclecart.databinding.ProductItemBinding b;
-
-        public ViewHolder(@NonNull ProductItemBinding b) {
+    // ViewHolder for Weight Based Product
+    public static class  WeightBasedProductViewHolder extends RecyclerView.ViewHolder {
+        WeightBasedProductBinding b;
+        public WeightBasedProductViewHolder(@NonNull WeightBasedProductBinding b){
             super(b.getRoot());
-            this.b = b;
+            this.b=b;
+        }
 
+    }
+
+    // ViewHolder for Variant Based Product
+    public static class  VariantBasedProductViewHolder extends RecyclerView.ViewHolder {
+        VariantBasedProductBinding b ;
+        public VariantBasedProductViewHolder(@NonNull VariantBasedProductBinding b){
+            super(b.getRoot());
+            this.b=b;
         }
     }
 }
