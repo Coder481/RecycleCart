fa4dcdbb8834b004bec0b3ddc2123ce1f6ef3acc
Coder481 <hrithiksharma24092000@gmail.com>
2020-11-08 10:25:31 +0530

    Update Commit

--- app/src/main/java/com/example/recyclecart/CatalogActivity.java
+++ app/src/main/java/com/example/recyclecart/CatalogActivity.java
@@ -1,10 +1,17 @@
 package com.example.recyclecart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import com.example.recyclecart.ProductsAdapter;
-import android.app.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.recyclecart.databinding.ActivityCatalogBinding;
 import com.example.recyclecart.models.Product;
@@ -16,14 +23,105 @@ import java.util.List;
 public class CatalogActivity extends AppCompatActivity {
 
     /**
-     * 1.
-     * 2.
-     * 3.
-     * 4.
-     * 5.
+     * 1. Create layout for each item
+     * 2. Add RecyclerView to the layout
+     * 3. Create Adapter for RecyclerView(RV)
+     * 4. Initialize the Adapter
+     * 5. Set the Adapter to RV
      */
 
     private ActivityCatalogBinding b;
+    private  ArrayList<Product> products;
+    private ProductsAdapter adapter;
+
+    // Inflating the menu resource
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_catalog_options,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // OnItem Click Listener
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_btn){
+            showProductEditorDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // OnClick handler for ContextualMenu of Product
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.contextualMenuEdit:
+                editLastSelectedItem();
+                return true;
+            case R.id.contextualMenuRemove:
+                removeLastSelectedItem();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    // Edit the details of Product
+    private void editLastSelectedItem() {
+        // Get data to be edited
+        Product lastSelectedProduct = products.get(adapter.lastSelectedItemPosition);
+
+        // Show editor dialog
+        new ProductEditorDialog()
+                .show(this, lastSelectedProduct, new ProductEditorDialog.OnProductEditedListener() {
+                    @Override
+                    public void onProductEdited(Product product) {
+                        // Replace old Data
+                        products.set(adapter.lastSelectedItemPosition,product);
+
+                        // Update View
+                        adapter.notifyItemChanged(adapter.lastSelectedItemPosition);
+                    }
+
+                    @Override
+                    public void onCancelled() {
+                        Toast.makeText(CatalogActivity.this, "Cancelled!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    // Remove the product
+    private void removeLastSelectedItem() {
+        new AlertDialog.Builder(this)
+                .setTitle("Do You really want to remove the Item?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        products.remove(adapter.lastSelectedItemPosition);
+                        adapter.notifyItemRemoved(adapter.lastSelectedItemPosition);
+                        Toast.makeText(CatalogActivity.this, "Item Removed!!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("CANCEL",null)
+                .show();
+    }
+
+
+    private void showProductEditorDialog() {
+        new ProductEditorDialog()
+                .show(this, new Product(), new ProductEditorDialog.OnProductEditedListener() {
+                    @Override
+                    public void onProductEdited(Product product) {
+                        products.add(product);
+                        adapter.notifyItemInserted(products.size()-1);
+                    }
+
+                    @Override
+                    public void onCancelled() {
+                        Toast.makeText(CatalogActivity.this,"Cancelled!!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +131,42 @@ public class CatalogActivity extends AppCompatActivity {
         b= ActivityCatalogBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
+
+
+        // Showing the Dialog of Weight Picker
+        b.showWeightPickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Weight Picker Dialog
+                WeightPicker.show(CatalogActivity.this, new WeightPicker.OnWeightPickedListener() {
+                    @Override
+                    public void onWeightPicked(int kg, int g) {
+                        Toast.makeText(CatalogActivity.this,"Picked values\n"+kg+" kg and "+g*50+" gm",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onWeightPickerCancelled() {
+                        Toast.makeText(CatalogActivity.this,"Cancelled!!",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+
         setupProductsList();
     }
 
     private void setupProductsList() {
         // Create DataSet
-        List<Product> products = new ArrayList<>(
-                Arrays.asList(
-                        new Product("Tomato",20)
-                        ,new Product("Potato",30)
-                        , new Product("Apple",100)
-                        ,new Product("Banana",35)
-                        ,new Product("Milk",55)
-                        , new Product("Cheese",70)
-                        ,new Product("Brocoli",50)
-                        ,new Product("Carrots",40)
-                        , new Product("Rice",60)
-                        ,new Product("Butter",65)
-                        ,new Product("Spinach",40)
-                        , new Product("Strawberries",120)
-                )
-        );
+        products = new ArrayList<>();
 
         // Create adapter object
-        ProductsAdapter adapter = new ProductsAdapter(this,products);
+        adapter = new ProductsAdapter(this,products);
 
         // Set the adapter & LayoutManager to recyclerView
         b.recyclerView.setAdapter(adapter);
         b.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        b.recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
     }
 }
